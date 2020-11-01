78def41357e79f71e805f61db997f63105fa9561
Swoshua <jplore@gmail.com>
2020-10-31 21:13:05 -0700

    almost done

---
+++ COMP4537/MemoryGame/backend/dataCall.js
@@ -0,0 +1,12 @@
+async function submit() {
+    let name = document.getElementById("name").value
+    let urlParams = new URLSearchParams(window.location.search)
+    let score = urlParams.get("score")
+    let url = `https://jl-memory-game.herokuapp.com/insert_score?name=${name}&${score}`
+    let res = await fetch(url)
+    return res.json()
+}
+
+async function leaderboards() {
+
+}
