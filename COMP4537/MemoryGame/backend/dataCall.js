fca8e351376630716870bfa14c614ea12739f865
Swoshua <jplore@gmail.com>
2020-10-31 22:27:31 -0700

    almost done

--- COMP4537/MemoryGame/backend/dataCall.js
+++ COMP4537/MemoryGame/backend/dataCall.js
@@ -1,7 +1,7 @@
 function submit() {
     let name = $("#username").text();
     let score = $("#final_score").text();
-    let url = `https://jl-memory-game.herokuapp.com/insert_score?name=${name}&${score}`
+    let url = `https://jl-memory-game.herokuapp.com/insert_score?name=${name}&score=${score}`
     fetch(url)
     .then(response => response.json())
     .then(data => console.log('success'));
